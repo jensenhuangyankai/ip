94b51b235e0bab9546fff8389437dba4f3940a85
jensenhuangyankai <jensen@huangyankai.com>
2025-01-24 11:00:24 +0800

    Add level 0

---
+++ src/main/java/gpt0.5.java
@@ -0,0 +1,15 @@
+public class Duke {
+    public static void main(String[] args) {
+        String helloString = """
+        ---------------------------------------------------
+        Hello! I'm GPT0.5.
+        What can I do for you today?
+        ---------------------------------------------------""";
+        String goodbyeString = """
+        ---------------------------------------------------
+        Goodbye! Have a nice day!
+        ---------------------------------------------------""";
+        System.out.println(helloString);
+        System.out.println(goodbyeString);
+    }
+}
